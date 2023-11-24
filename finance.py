a560063ab74545680af238c9cd150d355375c2d6
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-11-24 12:07:40 +0500

    kkk

--- finance.py
+++ finance.py
@@ -11,7 +11,18 @@ class Finance(Randommer):
         Returns:
             list: list of types
         '''
-        pass
+        endpoint="Finance/CryptoAddress/Types"
+        url=self.get_url()+endpoint  # https://randommer.io/api/Finance/CryptoAddress/Types
+
+        headers={
+            "X-Api-Key": api_key
+        }
+
+        response=requests.get(url, headers=headers)
+
+        if response.status_code==200:
+            return response.json()
+        return response.status_code
 
     def get_crypto_address(self, crypto_type: str, api_key: str) -> dict:
         '''get available crypto address
@@ -47,3 +58,6 @@ class Finance(Randommer):
             dict: idan data
         '''
         pass
+token = "2d794c6f46094ceb96bd719c1c26c984"
+card = Finance()
+print(card.get_crypto_address_types(api_key=token))
